37cef67ee67b799118a622b3db1541de792d9b6a
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-29 15:57:40 +0530

    UC1

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,13 @@
 public class EmpWageBuilder{
 	public static void main(String[] args){
-	
 		System.out.println("Welcome to Employee Wage Computation");
+		
+		int isFullTime = 1;
+		
+		double empCheck = Math.floor(Math.random()*10) % 2;
+		if(empCheck == isFullTime)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
-	
 }
