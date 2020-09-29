a09ea6732690eec6b410e45e5154b66f5eacb6f5
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-29 16:02:45 +0530

    UC2

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,12 +2,18 @@ public class EmpWageBuilder{
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
 		
-		int isFullTime = 1;
+		 int isFullTime = 1;
+		 int empRatePerHour = 20;
+		
+		int empHours = 0;
+		int empWage  = 0;
 		
 		double empCheck = Math.floor(Math.random()*10) % 2;
 		if(empCheck == isFullTime)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+			empHours = 8;
+		
+		empWage = empHours * empRatePerHour;
+		System.out.println("Emp Wage: " + empWage);
+
 	}
 }
