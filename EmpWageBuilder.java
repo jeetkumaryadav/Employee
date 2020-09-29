1fd59467edf61daa0a09bd3eadd3e701ef7c49f0
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-29 16:13:00 +0530

    UC4

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,19 +1,27 @@
 public class EmpWageBuilder{
+	
+	     public static final int isFullTime = 2;
+		 public static final int isPartTime = 1;
+		 public static final int empRatePerHour = 20;
+	
 	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation");
 		
-		 int isFullTime = 2;
-		 int isPartTime = 1;
-		 int empRatePerHour = 20;
+		System.out.println("Welcome to Employee Wage Computation");
 		
 		int empHours = 0;
 		int empWage  = 0;
 		
-		double empCheck = Math.floor(Math.random()*10) % 3;
-		if(empCheck == isFullTime)
-			empHours = 8;
-		else if(empCheck == isPartTime)
-			empHours = 4;
+		int empCheck = (int) Math.floor(Math.random()*10) % 3;
+		switch(empCheck){
+			case isFullTime:
+				empHours = 8;
+				break;
+			case isPartTime:
+				empHours = 4;
+				break;
+			default :
+				empHours = 0;
+		}
 		
 		empWage = empHours * empRatePerHour;
 		System.out.println("Emp Wage: " + empWage);
