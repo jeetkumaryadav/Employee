170c391bd8b666bdae59d95e21effee7c76c4cf1
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-29 16:55:28 +0530

    UC8

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,14 +2,10 @@ public class EmpWageBuilder{
 	
 	 public static final int isFullTime = 2;
 	 public static final int isPartTime = 1;
-	 public static final int empRatePerHour = 20;
-	 public static final int numOfWorkingDays = 20;
-	 public static final int maxWorkingHours = 100;
 	
-	public static int computeEmpWage(){
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours){
 		int empHours = 0; int totalWorkingHours = 0; int totalWorkingDays = 0;
-		
-		while(totalWorkingHours <maxWorkingHours && 
+		while(totalWorkingHours <=maxWorkingHours && 
 								totalWorkingDays < numOfWorkingDays) {
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
 			switch(empCheck){
@@ -27,11 +23,12 @@ public class EmpWageBuilder{
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: "+ totalWorkingHours);
 		} 
 		int totalEmpWage = totalWorkingHours*empRatePerHour;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		System.out.println("Total Emp Wage for Company: "+ company+" is: " + totalEmpWage);
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args){
-		computeEmpWage();
+		computeEmpWage("Dmart", 20, 2, 10);
+		computeEmpWage("Reliance", 10, 4, 20);
 	}
-}	
+}		
